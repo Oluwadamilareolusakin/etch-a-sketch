6476fc9690c44573329a0bb43687ab59a3102c0c
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-06-20 15:54:18 +0100

    Add draw functionality

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let numberofdivs= parseInt(prompt("Enter a measurement for your sketch pad, e.g 64x64"));
 
 const creatediv = (numberofdivs) =>{
-    
-    let w = window.innerWidth/(numberofdivs);
-    let h = window.innerHeight/(numberofdivs);
+    let container = document.querySelector('div#main-container');
+    let w = Math.floor(window.innerWidth/(numberofdivs));
+    let h = Math.floor(window.innerHeight/(numberofdivs));
     let div = document.createElement('div');
     div.classList.add('child-div');
     div.setAttribute('style', `border: 1px solid black;color:black; Height:${h}px; 
         Width:${w}px;padding:0; margin:0; background:rgba(0,0,0,0);`);
-    let container = document.querySelector('div#main-container');
+    
     
     container.append(div);
 }
@@ -25,11 +25,9 @@ const drawGird = (numberofdivs) =>{
 }
 
 const draw = (e) =>{
-    let color = e.target.style.backgroundColor;
-    console.log(color);
-    //let r,g,b = 255;
-    //r,g,b-=10;
-    //e.target.setAttribute('style',`background: rgb(${r},${g},${b})`)
+    let color =  e.target.style.background;
+    a = color[11]+=.2;
+    return e.target.style.background = `rgba(0,0,0,${a})`
 
 }
 
